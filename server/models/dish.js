0d66e4fedbaccbfcbe2c4cd119fe20b8fc0f788d
viccam <viccam@student.chalmers.se>
2022-09-14 11:27:01 +0200

    Add untested basic dish endpoints

---
+++ server/models/dish.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var dishSchema = new Schema ({
+    name: { type: String},
+    price: { type: Number}
+});
+module.exports = mongoose.model('dishes', dishSchema);
