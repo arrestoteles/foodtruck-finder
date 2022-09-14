d223aaf84915340411b0aec9bc118875dcff3b8f
balesic <balesic@student.chalmers.se>
2022-09-14 13:04:22 +0200

    Add GET and DELETE methods for customers

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -1,15 +1,56 @@
-var express = require("express");
-var router = express.Router();
-var Customer = require("../models/customer");
+var express = require('express')
+var router = express.Router()
+var Customer = require('../models/customer')
 
-router.post("/api/customers", function (req, res, next) {
-  var customer = new Customer(req.body);
-  customer.save(function (err, camel) {
+// Create a new customer
+router.post('/api/customers', function (req, res, next) {
+  var customer = new Customer(req.body)
+  customer.save(function (err, customer) {
     if (err) {
-      return next(err);
+      return next(err)
     }
-    res.status(201).json(customer);
-  });
-});
+    res.status(201).json(customer)
+  })
+})
 
-module.exports = router;
+// Return a list of all customers
+router.get('/api/customers', function (req, res, next) {
+  Customer.find(function (err, customers) {
+    if (err) {
+      return next(err)
+    }
+    res.json({ customers: customers })
+  })
+})
+
+// Return the customers with the given ID
+router.get('/api/customers/:id', function (req, res, next) {
+  var id = req.params.id
+  Customer.findById(id, function (err, customer) {
+    if (err) {
+      return next(err)
+    }
+    if (customer === null) {
+      return res.status(404).json({ message: 'Customer not found' })
+    }
+    res.json(customer)
+  })
+})
+
+// Delete the customer with the given ID
+router.delete('/api/customers/:id', function (req, res, next) {
+  var id = req.params.id
+  Customer.findOneAndDelete({ _id: id }, function (err, customer) {
+    if (err) {
+      return next(err)
+    }
+    if (customer === null) {
+      return res.status(404).json({ message: 'Customer not found' })
+    }
+    res.json(customer)
+  })
+})
+
+//TODO Update the customer with the given ID
+
+module.exports = router
--- server/models/customer.js
+++ server/models/customer.js
@@ -1,9 +1,9 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
 
 var customerSchema = new Schema({
   username: { type: String },
-  password: { type: Number },
-});
+  password: { type: String }
+})
 
-module.exports = mongoose.model("customers", customerSchema);
+module.exports = mongoose.model('customers', customerSchema)
