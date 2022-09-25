1ae1c4e96e6a6151eb29e59d4d6772ae6f1d268d
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-09-25 15:40:27 +0200

    Re-add foodtruck funationalities

---
+++ server/controllers/foodtrucks.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var router = express.Router();
+var Foodtruck = require('../models/foodtruck');
+
+//Create (post foodtruck)
+router.post('/', function (req, res, next) {
+    var foodtruck = new Foodtruck(req.body);
+    foodtruck.save(function (err, foodtruck) {
+        if (err) { return next(err); }
+        res.status(201).json(foodtruck);
+    })
+});
+
+
+
+//Create reminder for specific user
+router.post('/api/foodtrucks/:_id/dishes', function (req, res, next) {
+
+    var dish = new dish(req.body);
+    dish.user_id = req.params._id;
+    dish.save(function (err, dish) {
+        if (err) {
+            return next(err);
+        }
+        res.status(201).json(dish);
+    })
+});
+//Get all users
+router.get('/api/foodtrucks', function (req, res, next) {
+    foodtruck.find(function (err, users) {
+        if (err) { return next(err); }
+        res.json({ "foodtrucks": foodtrucks });
+    });
+});
+//Get all reminders of specific user
+router.get('/api/users/:_id/reminders', function (req, res, next) {
+    dish.find(function (err, dishes) {
+        if (err) { return next(err); }
+        res.json({ "dishes": dishes });
+    });
+});
+
+
+
+//Get all foodtrucks 
+router.get('/', function (req, res, next) {
+    Foodtruck.find(function (err, foodtrucks) {
+        if (err) { return next(err); }
+        res.json({ "foodtrucks": foodtrucks });
+    });
+});
+
+//Get food truck by ID (get individeual foodtruck by ID)
+router.get('/:_id', function (req, res, next) {
+    var id = req.params._id;
+    Foodtruck.findById(id, function (err, foodtrucks) {
+        if (err) { return next(err); }
+        if (foodtrucks == null) {
+            return res.status(404).json({ "message": "foodtruck not found" });
+        }
+        res.json(foodtrucks);
+    });
+});
+
+//Patch function by ID (Patch individual item )
+router.patch('/:_id', function (req, res, next) {
+    var id = req.params._id;
+    Foodtruck.findById(id, function (err, foodtrucks) {
+        if (err) { return next(err); }
+        if (foodtrucks == null) {
+            return res.status(404).json({ "message": "foodtrucks not found" });
+        }
+        foodtrucks.name = (req.body.name || foodtrucks.name);
+        foodtrucks.color = (req.body.color || foodtrucks.color);
+        foodtrucks.save();
+        res.json(foodtrucks);
+    });
+});
+
+
+//Delete individual foodtruck by ID
+router.delete('/:_id', function (req, res, next) {
+    var id = req.params._id;
+    Foodtruck.findOneAndDelete({ _id: id }, function (err, foodtrucks) {
+        if (err) { return next(err); }
+        if (foodtrucks == null) {
+            return res.status(404).json({ "message": "foodtruck not found" });
+        }
+        res.json(foodtrucks);
+    });
+});
+
+
+module.exports = router;
---
+++ server/models/foodtruck.js
@@ -0,0 +1,8 @@
+var Schema = mongoose.Schema;
+
+var foodtruckSchema = new Schema ({
+    name: { type: String },
+    color: { type: String },
+});
+
+module.exports = mongoose.model( 'foodtrucks', foodtruckSchema );
