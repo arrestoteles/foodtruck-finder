0edfc08e002bd9b6881138536f487cd345e4a748
balesic <balesic@student.chalmers.se>
2022-10-05 21:32:56 +0200

    Update forms on login and signup page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,35 +1,23 @@
 <template>
   <div>
     <b-form class="myForm" @submit="login" @reset="onReset" v-if="show">
-      <b-form-group id="input-group-1" label="Username:" label-for="input-1">
+      <b-form-group id="input-group-1" label="Email" label-for="input-1">
         <b-form-input
           id="input-1"
-          v-model="form.username"
-          type="username"
-          placeholder="Enter username"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter your email"
           required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+      <b-form-group id="input-group-2" label="Password" label-for="input-2">
         <b-form-input
           id="input-2"
           v-model="form.password"
-          placeholder="Enter password"
+          placeholder="Enter your password"
           required
-          description="Create your username here."
         ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          v-model="form.checked"
-          id="checkboxes-4"
-          :aria-describedby="ariaDescribedby"
-        >
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
       </b-form-group>
 
       <b-button type="submit" pill variant="primary">Login</b-button>
@@ -64,7 +52,7 @@ export default {
     onReset(event) {
       event.preventDefault()
       // Reset our form values
-      this.form.username = ''
+      this.form.email = ''
       this.form.password = ''
       // Trick to reset/clear native browser form validation state
       this.show = false
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,35 +1,41 @@
 <template>
   <div>
     <b-form class="myForm" @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="input-group-1" label="Username:" label-for="input-1">
+      <b-form-group id="input-group-1" label="First Name" label-for="input-1">
         <b-form-input
           id="input-1"
-          v-model="form.username"
-          type="username"
-          placeholder="Enter username"
+          v-model="form.firstname"
+          type="firstname"
+          placeholder="Enter your first name"
           required
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+      <b-form-group id="input-group-2" label="Last Name" label-for="input-2">
         <b-form-input
           id="input-2"
-          v-model="form.password"
-          placeholder="Enter password"
+          v-model="form.lastname"
+          placeholder="Enter your last name"
           required
-          description="Create your username here."
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-        <b-form-checkbox-group
-          v-model="form.checked"
-          id="checkboxes-4"
-          :aria-describedby="ariaDescribedby"
-        >
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
+      <b-form-group id="input-group-3" label="Email" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          v-model="form.email"
+          placeholder="Enter your email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="Password" label-for="input-4">
+        <b-form-input
+          id="input-4"
+          v-model="form.email"
+          placeholder="Enter a password"
+          required
+        ></b-form-input>
       </b-form-group>
 
       <b-button type="submit" pill variant="primary">Sign up</b-button>
@@ -64,7 +70,9 @@ export default {
     onReset(event) {
       event.preventDefault()
       // Reset our form values
-      this.form.username = ''
+      this.form.firstname = ''
+      this.form.lastname = ''
+      this.form.email = ''
       this.form.password = ''
       // Trick to reset/clear native browser form validation state
       this.show = false
