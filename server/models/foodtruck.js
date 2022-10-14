37ff9f79e6cd90f6b9c3362b66bd8b216bdcf360
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-10-14 11:05:34 +0200

    Create foodtruck and dishes relationship

--- server/models/foodtruck.js
+++ server/models/foodtruck.js
@@ -4,6 +4,9 @@ var Schema = mongoose.Schema;
 var foodtruckSchema = new Schema ({
     name: { type: String },
     color: { type: String },
+    dishes: { type: Schema.Types.ObjectId, ref: 'dishes' },
+    token: { type: String }
 });
+
 
 module.exports = mongoose.model( 'foodtrucks', foodtruckSchema );
