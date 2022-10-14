55e8ecb5f9342db482de6c9c4268b7d07b3a6618
Labiba Karar Eshaba <guseshka@student.gu.se>
2022-10-14 09:39:02 +0200

    Minor changes in the ComponentFood

--- client/src/components/ComponentFood.vue
+++ client/src/components/ComponentFood.vue
@@ -3,17 +3,8 @@
   <div class="card">
  <img src="https://thumbs.dreamstime.com/b/homemade-burger-fried-potatoes-french-fries-fast-food-set-delicious-fresh-tomatoes-lettuce-onions-cheese-rustic-wooden-table-93980191.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-     <div class="card-title">Food Truck</div>
-      <div class="card-text">Foodtruck name: {{ foodtruck.name }}</div>
-  <div class="accordion" role="tablist">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block v-b-toggle.accordion-1 variant="info">Dish Menu</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-        </b-card-body>
-      </b-collapse>
-  </div>
+     <div class="card-title">{{ foodtruck.name }}</div>
+      <div class="card-text">Dishes: </div>
 </div>
    </div>
    </div>
