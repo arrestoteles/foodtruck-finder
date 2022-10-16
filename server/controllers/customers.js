42636223e25260dc8a091558287669f42362e7e9
viccam <viccam@student.chalmers.se>
2022-10-16 09:08:14 +0200

    fix status code for delete id

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -195,7 +195,7 @@ router.delete('/:id', function (req, res, next) {
     if (customer === null) {
       return res.status(404).json({ message: 'Customer not found' })
     }
-    res.status(200).json(customer)
+    res.status(204).json(customer)
   })
 })
 
@@ -222,7 +222,7 @@ router.put('/:id', function(req, res, next) {
         customer.email = req.body.email
         customer.password = req.body.password
         customer.save()
-        res.status(204).json(customer)
+        res.status(200).json(customer)
     })
 })
 
@@ -239,7 +239,7 @@ router.patch('/:id', function(req, res, next) {
       customer.email = (req.body.email || customer.email)
       customer.password = (req.body.password || customer.password)
       customer.save()
-      res.status(204).json(customer)
+      res.status(200).json(customer)
   })
 })
 
