08267ad0c39ad827a9da3292eae42bed7ebdb92a
balesic <balesic@student.chalmers.se>
2022-10-18 19:53:04 +0200

    Refactor customer page

--- client/src/views/Customer.vue
+++ client/src/views/Customer.vue
@@ -7,6 +7,32 @@
       aria-controls="my-table"
     ></b-pagination>
 
+    <b-form
+      class="col-12 col-md-4"
+      @add.prevent="onAdd"
+      @reset.prevent="onReset"
+      v-if="show"
+    >
+      <b-form-group
+        class="formtxt"
+        id="input-group-1"
+        label="My Favorite Food Trucks"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.foodtruckform"
+          placeholder="Add your favorite food trucks here"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button class="addbtn" type="add" pill variant="success">Add </b-button>
+      <b-button class="resetbtn" type="reset" pill variant="outline-info"
+        >Reset</b-button
+      >
+    </b-form>
+
     <p class="mt-3">Current Page: {{ currentPage }}</p>
 
     <b-table
@@ -27,7 +53,11 @@ export default {
     return {
       perPage: 3,
       currentPage: 1,
-      foodtrucks: []
+      foodtrucks: [],
+      form: {
+        foodtruckform: ''
+      },
+      show: true
     }
   },
   computed: {
@@ -37,14 +67,10 @@ export default {
   },
   mounted() {
     console.log('Here is a list of all your foodtrucks!')
-    console.log('kladdkaka2')
     const customerId = localStorage.getItem('LoggedUser')
     const customerID = customerId.slice(1, -1)
-    alert(customerID)
     Api.get(`/customers/${customerID}/foodtrucks`)
       .then((response) => {
-        console.log(response)
-        console.log('kladdkaka1')
         this.foodtrucks = response.data.foodtrucks
         console.log(this.foodtrucks)
       })
@@ -55,6 +81,38 @@ export default {
       .then(() => {
         console.log('This runs every time after success or error.')
       })
+  },
+  methods: {
+    onReset() {
+      // Reset our form values
+      this.form.foodtruckform = 'Hungry much?'
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+.col-12 {
+  padding-top: 3%;
+  background-color: #6044ff;
+  margin: 0 auto;
+  width: 80%; /* value of your choice which suits your alignment */
+}
+.addbtn {
+  margin-right: 5px;
+  margin-bottom: 10px;
+}
+.resetbtn {
+  margin-left: 5px;
+  margin-bottom: 10px;
+}
+.formtxt {
+  color: whitesmoke;
+  font-size: 250%;
+}
+</style>
